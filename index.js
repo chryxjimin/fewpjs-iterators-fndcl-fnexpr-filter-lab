542bf1eade405d89774a9007e83afad65cd9d3d2
Your Username <you@example.com>
2021-02-19 22:38:09 -0500

    commit tree b676a93c92e426b1b66b120dd38cb4a284821447

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(currentDriver, matchingDriver) {
+    return currentDriver.filter( possibleMatch =>
+        possibleMatch.toLowerCase() === matchingDriver.toLowerCase()
+    )
+}
+
+function fuzzyMatch(currentDriver, matchingCharacters) {
+    return currentDriver.filter( possibleMatch =>
+        possibleMatch.toLowerCase().indexOf(matchingCharacters.toLowerCase()) === 0
+    )
+}
+
+
+function matchName(currentDriver, matchingDriver) {
+    return currentDriver.filter( record => record.name === matchingDriver)
+}
